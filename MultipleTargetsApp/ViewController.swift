269ab85c9c287efa81d0b5ce2b18cdf43dc4cee7
Petar Glisovic <petar.glisovic@gecko.rs>
2019-07-16 19:06:27 +0200

    Setup project

---
+++ MultipleTargetsApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  MultipleTargetsApp
+//
+//  Created by Petar Glisovic on 7/10/19.
+//  Copyright © 2019 Petar Glisovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var backgroundView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+    
+}
